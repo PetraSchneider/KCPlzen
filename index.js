75d3dc179eed0d6c01495d0a7f6fcd7208efc7d8
Petra Schneiderova <pe.schneiderova@gmail.com>
2023-05-18 15:08:55 +0200

    pokracovani

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function toggleAccordion(event) {
+    var accordion = event.target.closest('.accordion');
+    var accordionContent = accordion.querySelector('.accordion-content');
+  
+    accordion.classList.toggle('active');
+    accordionContent.style.display = accordion.classList.contains('active') ? 'block' : 'none';
+  }
